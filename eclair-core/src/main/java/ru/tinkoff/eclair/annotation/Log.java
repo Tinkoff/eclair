52232e96ca95241091d4f1cc6099e21af9bea140
klapatnyuk <klapatnyuk@yandex.ru>
2018-04-08 19:55:51 +0300

    Corrected @Log's Javadoc

--- eclair-core/src/main/java/ru/tinkoff/eclair/annotation/Log.java
+++ eclair-core/src/main/java/ru/tinkoff/eclair/annotation/Log.java
@@ -15,11 +15,11 @@ import java.lang.reflect.Parameter;
 import static org.springframework.boot.logging.LogLevel.*;
 
 /**
- * Specifies need to log beginning and ending (but not emergency ending) of annotated {@link Method} execution.
- * Works similarly to definition simultaneously {@link in} and {@link out} annotations with all matching attribute values.
- * Note: emergency ending of method execution should be specified separately by {@link error} annotation.
+ * Annotated {@link Method} is able to log beginning and ending (except the emergency ending) of execution.
+ * Works the same as both {@link in} and {@link out} annotations with all matching attribute values.
+ * Note: emergency ending of the method execution should be specified separately by {@link error} annotation.
  *
- * Could be defined on {@link Parameter} and specify settings of logging for it.
+ * Can be defined on {@link Parameter} and specify logging settings for it.
  *
  * Should have unique {@link #logger} value per annotated element.
  * {@link Parameter}-level annotation has higher priority settings than {@link Method}-level with same {@link #logger} value.
@@ -39,7 +39,7 @@ public @interface Log {
     LogLevel value() default DEBUG;
 
     /**
-     * Expected level for log beginning and ending of method execution.
+     * Expected level to log beginning and ending of method execution.
      */
     @AliasFor("value")
     LogLevel level() default DEBUG;
@@ -51,7 +51,7 @@ public @interface Log {
     LogLevel ifEnabled() default OFF;
 
     /**
-     * If specified log-level is enabled for the current {@link #logger}, activates detailed logging.
+     * If specified log-level is enabled for the current {@link #logger} activates detailed logging.
      * For annotated {@link Method} verbose log includes argument/return values.
      * For annotated {@link Parameter} verbose log includes argument name.
      * Note: it is assumed that {@link LogLevel#OFF} deactivates verbose logging of annotated element for any level.
@@ -60,22 +60,22 @@ public @interface Log {
 
     /**
      * Determines {@link Printer} implementation by specified bean name (or alias).
-     * Found printer will be used for converting argument/return values from raw type to {@link String}.
-     * Note: if not specified, highest priority compatible printer or {@link PrinterResolver#defaultPrinter} will be used.
+     * The printer will be used to convert argument/return values from raw type to {@link String}.
+     * Note: if not specified highest priority compatible printer or {@link PrinterResolver#defaultPrinter} will be used.
      *
      * @see Order
      */
     String printer() default "";
 
     /**
-     * Determines {@link EclairLogger} implementation by specified bean name (or alias), which should process this annotation.
-     * Note: if not specified, single candidate or {@link Primary} bean will be used for processing.
+     * Determines {@link EclairLogger} implementation by specified bean name (or alias) which should process this annotation.
+     * Note: if not specified single candidate or {@link Primary} bean will be used for processing.
      */
     // TODO: convert to logger array everywhere?
     String logger() default "";
 
     /**
-     * Specifies need to log beginning (but not ending) of annotated {@link Method} execution.
+     * Annotated {@link Method} is able to log only beginning of execution.
      * Should be specified with unique {@link #logger} value per method.
      */
     @Repeatable(ins.class)
@@ -90,7 +90,7 @@ public @interface Log {
         LogLevel value() default DEBUG;
 
         /**
-         * Expected level for log beginning of method execution.
+         * Expected level to log beginning of method execution.
          */
         @AliasFor("value")
         LogLevel level() default DEBUG;
@@ -102,7 +102,7 @@ public @interface Log {
         LogLevel ifEnabled() default OFF;
 
         /**
-         * If specified log-level is enabled for the current {@link #logger}, activates detailed logging.
+         * If specified log-level is enabled for the current {@link #logger} activates detailed logging.
          * Verbose log includes method argument values.
          * Note: it is assumed that {@link LogLevel#OFF} deactivates verbose logging of annotated method for any level.
          */
@@ -110,22 +110,22 @@ public @interface Log {
 
         /**
          * Determines {@link Printer} implementation by specified bean name (or alias).
-         * Found printer will be used for converting method argument values from raw type to {@link String}.
-         * Note: if not specified, highest priority compatible printer or {@link PrinterResolver#defaultPrinter} will be used.
+         * The printer will be used to convert method argument values from raw type to {@link String}.
+         * Note: if not specified highest priority compatible printer or {@link PrinterResolver#defaultPrinter} will be used.
          *
          * @see Order
          */
         String printer() default "";
 
         /**
-         * Determines {@link EclairLogger} implementation by specified bean name (or alias), which should process this annotation.
-         * Note: if not specified, single candidate or {@link Primary} bean will be used for processing.
+         * Determines {@link EclairLogger} implementation by specified bean name (or alias) which should process this annotation.
+         * Note: if not specified single candidate or {@link Primary} bean will be used for processing.
          */
         String logger() default "";
     }
 
     /**
-     * Container for annotating method by multiple repeatable {@link in} annotations.
+     * Container to annotate method by multiple repeatable {@link in} annotations.
      *
      * @see Repeatable
      */
@@ -137,7 +137,7 @@ public @interface Log {
     }
 
     /**
-     * Specifies need to log ending (but not beginning) of annotated {@link Method} execution.
+     * Annotated {@link Method} is able to log only ending of execution.
      * Should be specified with unique {@link #logger} value per method.
      */
     @Repeatable(outs.class)
@@ -152,7 +152,7 @@ public @interface Log {
         LogLevel value() default DEBUG;
 
         /**
-         * Expected level for log ending of method execution.
+         * Expected level to log ending of method execution.
          */
         @AliasFor("value")
         LogLevel level() default DEBUG;
@@ -164,7 +164,7 @@ public @interface Log {
         LogLevel ifEnabled() default OFF;
 
         /**
-         * If specified log-level is enabled for the current {@link #logger}, activates detailed logging.
+         * If specified log-level is enabled for the current {@link #logger} activates detailed logging.
          * Verbose log includes method return value.
          * Note: it is assumed that {@link LogLevel#OFF} deactivates verbose logging of annotated method for any level.
          */
@@ -172,22 +172,22 @@ public @interface Log {
 
         /**
          * Determines {@link Printer} implementation by specified bean name (or alias).
-         * Found printer will be used for converting method return value from raw type to {@link String}.
-         * Note: if not specified, highest priority compatible printer or {@link PrinterResolver#defaultPrinter} will be used.
+         * The printer will be used to convert method return value from raw type to {@link String}.
+         * Note: if not specified highest priority compatible printer or {@link PrinterResolver#defaultPrinter} will be used.
          *
          * @see Order
          */
         String printer() default "";
 
         /**
-         * Determines {@link EclairLogger} implementation by specified bean name (or alias), which should process this annotation.
-         * Note: if not specified, single candidate or {@link Primary} bean will be used for processing.
+         * Determines {@link EclairLogger} implementation by specified bean name (or alias) which should process this annotation.
+         * Note: if not specified single candidate or {@link Primary} bean will be used for processing.
          */
         String logger() default "";
     }
 
     /**
-     * Container for annotating method by multiple repeatable {@link out} annotations.
+     * Container to annotate method by multiple repeatable {@link out} annotations.
      *
      * @see Repeatable
      */
@@ -214,7 +214,7 @@ public @interface Log {
         LogLevel value() default ERROR;
 
         /**
-         * Expected level for logging emergency ending of method execution (interrupted by thrown {@link Throwable} or its child).
+         * Expected level to log emergency ending of method execution (interrupted by thrown {@link Throwable} or its child).
          */
         @AliasFor("value")
         LogLevel level() default ERROR;
@@ -226,35 +226,35 @@ public @interface Log {
         LogLevel ifEnabled() default OFF;
 
         /**
-         * If specified log-level is enabled for the current {@link #logger}, activates detailed logging.
+         * If specified log-level is enabled for the current {@link #logger} activates detailed logging.
          * Verbose log includes {@link Throwable#toString()} value.
          * Note: it is assumed that {@link LogLevel#OFF} deactivates verbose logging of annotated method for any level.
          */
         LogLevel verbose() default ERROR;
 
         /**
-         * Determines array of {@link Throwable} types (with children), which should be processed by this annotation.
+         * Determines array of {@link Throwable} types (with children) which should be processed by this annotation.
          * Every thrown {@link Throwable} could be processed by only one annotation per {@link #logger}.
          * Tries to process any {@link Throwable} by default.
          */
         Class<? extends Throwable>[] ofType() default Throwable.class;
 
         /**
-         * Determines filter of {@link Throwable} types (with children), which should NOT be processed by this annotation.
+         * Determines filter of {@link Throwable} types (with children) which should NOT be processed by this annotation.
          * Note: 'exclude' filter has higher priority than 'ofType' defined by {@link #ofType}:
-         * so type contained simultaneously on {@link #ofType} and 'exclude' arrays will NOT be processed by this annotation.
+         * so type contained in both {@link #ofType} and 'exclude' arrays will NOT be processed by this annotation.
          */
         Class<? extends Throwable>[] exclude() default {};
 
         /**
-         * Determines {@link EclairLogger} implementation by specified bean name (or alias), which should process this annotation.
-         * Note: if not specified, single candidate or {@link Primary} bean will be used for processing.
+         * Determines {@link EclairLogger} implementation by specified bean name (or alias) which should process this annotation.
+         * Note: if not specified single candidate or {@link Primary} bean will be used for processing.
          */
         String logger() default "";
     }
 
     /**
-     * Container for annotating method by multiple repeatable {@link error} annotations.
+     * Container to annotate method by multiple repeatable {@link error} annotations.
      *
      * @see Repeatable
      */
